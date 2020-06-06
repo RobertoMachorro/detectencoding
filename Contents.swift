5cad34e9ef11f3dfff3831ac0a74e435e9330394
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2020-06-05 23:04:35 -0400

    Added test/sample code for detecting encoding from resource text file.

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,24 @@
 import Cocoa
 
-var str = "Hello, playground"
+if let fileURL = Bundle.main.url(forResource: "test", withExtension: "txt"),
+	let data = try? NSData(contentsOf: fileURL) as Data {
+
+	// Using NSString.stringEncoding
+
+	var convertedString: NSString?
+	let encoding = NSString.stringEncoding(for: data, encodingOptions: nil, convertedString: &convertedString, usedLossyConversion: nil)
+	if let text = convertedString as String? {
+		let encodingName = String.Encoding.init(rawValue: encoding)
+		print("[\(text)] \(encodingName)")
+	}
+
+	// By trial and error
+
+	if let text = String(data: data, encoding: .utf8) {
+		print("[\(text)] UTF8")
+	} else if let text = String(data: data, encoding: .macOSRoman) {
+		print("[\(text)] macOSRoman")
+	} else if let text = String(data: data, encoding: .ascii) {
+		print("[\(text)] ASCII")
+	}
+}
